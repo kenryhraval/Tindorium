70082d67c36ff3546a4f22bfe6790937df9075d4
Henrijs Kravals <henrijskravals98@gmail.com>
2023-12-05 10:48:23 +0200

    5. decembris

---
+++ Controllers/UserController.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Tindorium.API.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class UserController : ControllerBase
+    {
+        [HttpGet]
+        [Route("Ping")]
+        public string Ping()
+        {
+            return "PONG";
+        }
+
+        [HttpGet]
+        [Route("Pong")]
+        public string Pong()
+        {
+            return "PING";
+        }
+        
+        [HttpGet]
+        [Route ("GetUser")]
+        public string GetUser(string id)
+        {
+            return "pārstrādāts " + id;
+        }
+
+        
+    }
+}
